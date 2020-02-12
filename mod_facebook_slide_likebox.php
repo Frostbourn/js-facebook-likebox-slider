88479677be2d7e3167c0543b6411d1922620bdf9
Frostbourn <frostbourn@gmail.com>
2020-02-12 13:33:14 +0100

    Styling changes

---
+++ mod_facebook_slide_likebox.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package    JS Like Box Slider
+ * @license    GNU/GPL http://www.gnu.org/copyleft/gpl.html
+ * @link       https://jsns.eu
+ */
+ 
+//no direct access
+defined('_JEXEC') or die('Direct Access to this location is not allowed.');
+
+require( JModuleHelper::getLayoutPath( 'mod_facebook_slide_likebox' ) );
+?>
