82262b82b26ac3efe680e6671cd3c22baa58f438
Frostbourn <frostbourn@gmail.com>
2020-02-14 14:09:27 +0100

    Added switch instead of if statements

--- tmpl/default.php
+++ tmpl/default.php
@@ -17,58 +17,47 @@ class modSlideLikebox
 		global $mainframe;
 		$document = JFactory::getDocument();
 		$document->addStyleSheet(JURI::root() . 'modules/mod_facebook_slide_likebox/tmpl/css/style.min' . '.css', 'text/css', null, array());
+		$facebook_id = $params->get('facebook_login');
+		$twitter_id =  $params->get('twitter_login');
 
 		#______________________MOBILE________________________
-
 		if (trim($params->get('show_on_mobile')) == 1) 
 		{
-
-			$iPhone  = stripos($_SERVER['HTTP_USER_AGENT'], "iPhone");
-			$iPad    = stripos($_SERVER['HTTP_USER_AGENT'], "iPad");
-			$Android = stripos($_SERVER['HTTP_USER_AGENT'], "Android");
-
-			if ($iPhone || $iPad) 
-			{
-				$fb_url = 'fb://profile/' . $params->get('profile_id');
-			} else if ($Android) {
-				$fb_url = 'fb://page/' . $params->get('profile_id');
-			}
-
-			if (trim($params->get('facebook')) == 1) {
-				$fb = 1;
-			} else {
-				$fb = 0;
-			}
-
-			if (trim($params->get('twitter')) == 1) {
-				$tw = 1;
-			} else {
-				$tw = 0;
-			}
-
-			$sum = $fb + $tw;
-			$mobile_style = '#social_mobile a {position: relative;float: left; width: calc(100% / ' . $sum . ');display:list-item; list-style-type: none;} #social_mobile a:focus, #social_mobile a:hover { width: calc(100% / ' . $sum . ');-moz-transition-property: none; -webkit-transition-property: none; -o-transition-property: none;transition-property: none;}';
-			$document->addStyleDeclaration($mobile_style); 
-
 			?>
 			<div id="social_mobile">
 				<div class="top-left">
 				<?php
-					if (trim($params->get('facebook')) == 1) 
-					{ 	
-						?>
-						<a class="facebook pop-upper" href="<?php echo $fb_url ?>" target="_blank">
-							<i class="fa fa-facebook-f"></i>
-						</a>
-						<?php 
-					}
-					if (trim($params->get('twitter')) == 1) 
-					{ 
-						?>
-						<a class="twitter pop-upper" href="https://twitter.com/<?php echo $params->get('twitter_login'); ?>" target="_blank">
-							<i class="fa fa-twitter"></i>
-						</a>
-						<?php 
+					switch(true)
+					{
+						case (!empty($facebook_id)):
+							$fb = 1;
+							$iPhone  = stripos($_SERVER['HTTP_USER_AGENT'], "iPhone");
+							$iPad    = stripos($_SERVER['HTTP_USER_AGENT'], "iPad");
+							$Android = stripos($_SERVER['HTTP_USER_AGENT'], "Android");
+				
+							if ($iPhone || $iPad) 
+							{
+								$fb_url = 'fb://profile/' . $facebook_id;
+							} else if ($Android) {
+								$fb_url = 'fb://page/' . $facebook_id;
+							}
+								?>
+								<a class="facebook pop-upper" href="<?php echo $fb_url ?>" target="_blank">
+									<i class="fa fa-facebook-f"></i>
+								</a>
+								<?php 
+						case (!empty($twitter_id)):
+							$tw = 1;
+							?>
+							<a class="twitter pop-upper" href="https://twitter.com/<?php echo $twitter_id ?>" target="_blank">
+								<i class="fa fa-twitter"></i>
+							</a>
+							<?php 
+						default:
+							$sum = $fb + $tw;
+							$mobile_style = '#social_mobile a {position: relative;float: left; width: calc(100% / ' . $sum . ');display:list-item; list-style-type: none;} #social_mobile a:focus, #social_mobile a:hover { width: calc(100% / ' . $sum . ');-moz-transition-property: none; -webkit-transition-property: none; -o-transition-property: none;transition-property: none;}';
+							$document->addStyleDeclaration($mobile_style); 
+						break;
 					} 
 				?>
 				</div>
@@ -77,64 +66,65 @@ class modSlideLikebox
 		}
 
 		#______________________DESKTOP________________________
-
-		if (trim($params->get('position')) == 1) 
-		{
-			if (trim($params->get('buttons_shape')) == 1) 
-			{
-				$buttons_shape = '.social_slider .facebook_icon, .social_slider .twitter_icon {border-radius: 0 7px 7px 0 !important;}';
-				$document->addStyleDeclaration($buttons_shape);
-			}
-			$position_left = '.social_slider {position: fixed; left:-370px; z-index:99997; transition: all .5s .2s;}.social_slider:hover{transition: all .5s .2s; transform: translateX(370px);}.social_slider .facebook_icon{float:right;background-color: #3289d9;right:-31px; clear: right;}.social_slider .twitter_icon{float:right; clear: right;background-color: #27a4d9;right:-31px}';
-			$document->addStyleDeclaration($position_left);
-		} else if (trim($params->get('position')) == 0) 
-		{
-			if (trim($params->get('buttons_shape')) == 1) 
-			{
-				$buttons_shape = '.social_slider .facebook_icon, .social_slider .twitter_icon {border-radius: 7px 0 0 7px !important;}';
-				$document->addStyleDeclaration($buttons_shape);
-			}
-			$position_right = '.social_slider {position: fixed; right:-370px; z-index:99997; transition: all .5s .2s;}.social_slider:hover{transition: all .5s .2s; transform: translateX(-370px);} .social_slider .facebook_icon{float:left;left:-31px; background-color: #3289d9; clear: left;}.social_slider .twitter_icon{float:left;left:-31px; background-color: #27a4d9; clear: left;}';
-			$document->addStyleDeclaration($position_right);
-		} 
-
 		?>
 		<div class="social_slider" style="top: <?php echo $params->get('margintop') ?> !important;">
 		<?php
-			if (trim($params->get('facebook')) == 1) 
-			{ 
-				?>
-				<input id="tab1" type="radio" name="tabs" checked />
-				<label for="tab1" class="facebook_icon" style="max-width: 32px;"><span>facebook</span><i class="fa fa-facebook-f"></i></label>
-				<section id="content1">
-					<div class="facebook_box">
-						<iframe src="https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/<?php echo $params->get('profile_id'); ?>&tabs=timeline,events,messages&width=350&height=470&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true" width="350" height="470" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true">
-						</iframe>
+			switch (true)
+			{
+				case (!empty($facebook_id)):
+					?>
+					<input id="tab1" type="radio" name="tabs" checked />
+					<label for="tab1" class="facebook_icon" style="max-width: 32px;"><span>facebook</span><i class="fa fa-facebook-f"></i></label>
+					<section id="content1">
+						<div class="facebook_box">
+							<iframe src="https://www.facebook.com/plugins/page.php?href=https://www.facebook.com/<?php echo $params->get('facebook_login'); ?>&tabs=timeline,events,messages&width=350&height=470&small_header=false&adapt_container_width=false&hide_cover=false&show_facepile=true" width="350" height="470" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true">
+							</iframe>
+						</div>
+					</section>
+					<?php 
+				case (!empty($twitter_id)):
+					?>
+					<input id="tab2" type="radio" name="tabs" />
+					<label for="tab2" class="twitter_icon" style="max-width: 32px;"><span>twitter</span><i class="fa fa-twitter"></i></label>
+					<section id="content2">
+						<div class="twitter_box">
+							<a class="twitter-timeline" data-width="350" data-height="470" href="https://twitter.com/<?php echo $twitter_id ?>">Tweets by <?php echo $twitter_id ?></a>
+							<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+						</div>
+					</section>
+					<?php
+				default:
+					if (trim($params->get('position')) == 1) 
+					{
+						if (trim($params->get('buttons_shape')) == 1) 
+						{
+							$buttons_shape = '.social_slider .facebook_icon, .social_slider .twitter_icon {border-radius: 0 7px 7px 0 !important;}';
+							$document->addStyleDeclaration($buttons_shape);
+						}
+						$position_left = '.social_slider {position: fixed; left:-370px; z-index:99997; transition: all .5s .2s;}.social_slider:hover{transition: all .5s .2s; transform: translateX(370px);}.social_slider .facebook_icon{float:right;right:-31px; clear: right;}.social_slider .twitter_icon{float:right; clear: right;right:-31px}';
+						$document->addStyleDeclaration($position_left);
+					} else if (trim($params->get('position')) == 0) 
+					{
+						if (trim($params->get('buttons_shape')) == 1) 
+						{
+							$buttons_shape = '.social_slider .facebook_icon, .social_slider .twitter_icon {border-radius: 7px 0 0 7px !important;}';
+							$document->addStyleDeclaration($buttons_shape);
+						}
+						$position_right = '.social_slider {position: fixed; right:-370px; z-index:99997; transition: all .5s .2s;}.social_slider:hover{transition: all .5s .2s; transform: translateX(-370px);} .social_slider .facebook_icon{float:left;left:-31px; clear: left;}.social_slider .twitter_icon{float:left;left:-31px; clear: left;}';
+						$document->addStyleDeclaration($position_right);
+					} 
+					?>
+					<div class="copyrightlink">Designed with 
+						<span style="color: #f44336;">❤</span> by 
+						<a title="Joomla Extensions" target="_blank" href="https://jsns.eu" rel="noopener noreferrer">jsns.eu</a>
+						</div>
 					</div>
-				</section>
-				<?php 
+				<?php
+				break;
 			}
-
-			if (trim($params->get('twitter')) == 1) 
-			{ 
-				?>
-				<input id="tab2" type="radio" name="tabs" />
-				<label for="tab2" class="twitter_icon" style="max-width: 32px;"><span>twitter</span><i class="fa fa-twitter"></i></label>
-				<section id="content2">
-					<div class="twitter_box">
-						<a class="twitter-timeline" data-width="350" data-height="470" href="https://twitter.com/<?php echo $params->get('twitter_login'); ?>">Tweets by <?php echo $params->get('twitter_login'); ?></a>
-						<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-					</div>
-				</section>
-				<?php 
-			} 
-			?>
-			<div class="copyrightlink">Designed with 
-				<span style="color: #f44336;">❤</span> by 
-				<a title="Joomla Extensions" target="_blank" href="https://jsns.eu" rel="noopener noreferrer">jsns.eu</a>
-			</div>
+		?>
 		</div>
-		<?php
+		<?php		
 	}
 }
 ?>
